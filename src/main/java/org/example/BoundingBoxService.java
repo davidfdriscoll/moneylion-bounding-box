4a78256de9386a7244c3a27bf83aa7cfe432979e
David F. Driscoll <david.f.driscoll@gmail.com>
2024-06-17 22:47:44 -0400

    add nonoverlapping rectangles

--- src/main/java/org/example/BoundingBoxService.java
+++ src/main/java/org/example/BoundingBoxService.java
@@ -2,33 +2,75 @@ package org.example;
 
 import java.util.*;
 
+import com.google.common.collect.Range;
+import com.google.common.collect.RangeMap;
+import com.google.common.collect.TreeRangeMap;
+
 public class BoundingBoxService {
+    public static Set<BoundingBox> findNonOverlappingBoundingBoxes(List<BoundingBox> boxes) {
+        List<Event> events = new LinkedList<>();
+
+        for (BoundingBox box: boxes) {
+            int x1 = box.topLeft().x();
+            int x2 = box.bottomRight().x();
+            int y1 = box.topLeft().y();
+            int y2 = box.bottomRight().y();
+            events.add(new Event(x1, y1, y2, true, box));
+            events.add(new Event(x2, y1, y2, false, box));
+        }
+
+        Collections.sort(events);
+
+        Set<BoundingBox> nonOverlappingBoundingBoxes = new HashSet<>();
+        RangeMap<Integer, BoundingBox> intervals
+                = TreeRangeMap.create();
+
+        for (Event event: events) {
+            Range<Integer> range = Range.closed(event.y1(), event.y2());
+            if (event.isStart()) {
+                RangeMap<Integer, BoundingBox> overlaps = intervals.subRangeMap(range);
+                Collection<BoundingBox> overlappingBoxes = overlaps.asMapOfRanges().values();
+                if (overlappingBoxes.isEmpty()) {
+                    nonOverlappingBoundingBoxes.add(event.box());
+                } else {
+                    nonOverlappingBoundingBoxes.removeAll(overlappingBoxes);
+                }
+                intervals.put(range, event.box());
+            } else {
+                intervals.remove(range);
+            }
+        }
+
+        return nonOverlappingBoundingBoxes;
+    }
+
     public static List<BoundingBox> findBoundingBoxes(boolean[][] grid) {
         int numRows = grid.length;
         int numCols = grid[0].length;
 
         List<BoundingBox> boundingBoxes = new LinkedList<>();
 
+        Set<Node> directions = new HashSet<>();
+        directions.add(new Node(1, 0));
+        directions.add(new Node(0, 1));
+        directions.add(new Node(-1, 0));
+        directions.add(new Node(0, -1));
+
         for (int col = 0; col < numCols; col++) {
             for (int row = 0; row < numRows; row++) {
                 if (grid[row][col]) {
                     Node node = new Node(col, row);
-                    boundingBoxes.add(traverseGrid(grid, node));
+                    BoundingBox box = traverseGrid(grid, node, directions);
+                    boundingBoxes.addLast(box);
                 }
             }
         }
         return boundingBoxes;
     }
 
-    private static BoundingBox traverseGrid(boolean[][] grid, Node startNode) {
+    private static BoundingBox traverseGrid(boolean[][] grid, Node startNode, Set<Node> directions) {
         int numRows = grid.length;
         int numCols = grid[0].length;
-
-        Set<Node> directions = new HashSet<>();
-        directions.add(new Node(1, 0));
-        directions.add(new Node(0, 1));
-        directions.add(new Node(-1, 0));
-        directions.add(new Node(0, -1));
 
         Deque<Node> queue = new LinkedList<>();
         queue.add(startNode);
--- src/test/java/org/example/BoundingBoxServiceTest.java
+++ src/test/java/org/example/BoundingBoxServiceTest.java
@@ -2,15 +2,17 @@ package org.example;
 
 import org.junit.Test;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BoundingBoxServiceTest {
     @Test
     public void testFindBoundingBoxesSimple() {
-        boolean[][] array = TestResources.simpleArray;
+        boolean[][] array = TestResources.getSimpleArray();
         List<BoundingBox> boundingBoxes = BoundingBoxService.findBoundingBoxes(array);
 
         List<BoundingBox> expectedBoundingBoxes = new LinkedList<>();
@@ -21,7 +23,7 @@ public class BoundingBoxServiceTest {
 
     @Test
     public void testFindBoundingBoxesGroups() {
-        boolean[][] array = TestResources.groupsArray;
+        boolean[][] array = TestResources.getGroupsArray();
         List<BoundingBox> boundingBoxes = BoundingBoxService.findBoundingBoxes(array);
 
         List<BoundingBox> expectedBoundingBoxes = new LinkedList<>();
@@ -30,5 +32,31 @@ public class BoundingBoxServiceTest {
         expectedBoundingBoxes.add(new BoundingBox(new Node(8, 0), new Node(11, 2)));
 
         assertEquals(expectedBoundingBoxes, boundingBoxes);
+    }
+
+    @Test
+    public void testFindNonOverlappingBoundingBoxesSimple() {
+        boolean[][] array = TestResources.getSimpleArray();
+        List<BoundingBox> boundingBoxes = BoundingBoxService.findBoundingBoxes(array);
+        Set<BoundingBox> nonOverlapping =
+            BoundingBoxService.findNonOverlappingBoundingBoxes(boundingBoxes);
+
+        Set<BoundingBox> expectedBoundingBoxes = new HashSet<>();
+        expectedBoundingBoxes.add(new BoundingBox(new Node(1, 1), new Node(2,2)));
+
+        assertEquals(expectedBoundingBoxes, nonOverlapping);
+    }
+
+    @Test
+    public void testFindNonOverlappingBoundingBoxesGroups() {
+        boolean[][] array = TestResources.getGroupsArray();
+        List<BoundingBox> boundingBoxes = BoundingBoxService.findBoundingBoxes(array);
+        Set<BoundingBox> nonOverlapping =
+            BoundingBoxService.findNonOverlappingBoundingBoxes(boundingBoxes);
+
+        Set<BoundingBox> expectedBoundingBoxes = new HashSet<>();
+        expectedBoundingBoxes.add(new BoundingBox(new Node(0, 0), new Node(1,1)));
+
+        assertEquals(expectedBoundingBoxes, nonOverlapping);
     }
 }
