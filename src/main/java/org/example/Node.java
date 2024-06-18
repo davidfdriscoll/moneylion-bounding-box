34289e41c269011a20564be0b4524646589f3b0e
David F. Driscoll <david.f.driscoll@gmail.com>
2024-06-17 23:34:22 -0400

    run from command line

--- src/main/java/org/example/Node.java
+++ src/main/java/org/example/Node.java
@@ -1,3 +1,8 @@
 package org.example;
 
-public record Node(int x, int y) {}
+public record Node(int x, int y) {
+    @Override
+    public String toString() {
+        return "(" + (x + 1) + "," + (y + 1) + ")";
+    }
+}
